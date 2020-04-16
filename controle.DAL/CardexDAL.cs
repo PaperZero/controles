057f516e84938dd8627a3f2e6f7609de187a4354
PaperZero <63213770+PaperZero@users.noreply.github.com>
2020-04-16 05:09:11 -0300

    Add files via upload

---
+++ controle.DAL/CardexDAL.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace controle.DAL
+{
+    public class CardexDAL
+    {
+        public int IdCardex { get; set; }
+        public int IdProduto { get; set; }
+        public string Produto { get; set; }
+        public int Quantidade { get; set; }
+        public DateTime Entrada { get; set; }
+        public DateTime Validade { get; set; }
+        public DateTime Saida { get; set; }
+        public List<CardexDAL> listacardex { get; set; }
+
+
+
+        private SqlConnection conn;
+
+        string caminho = "workstation id=controle.mssql.somee.com;packet size=4096;user id=Papel_zero_SQLLogin_1;pwd=k9nni8mvty;data source=controle.mssql.somee.com;persist security info=False;initial catalog=controle";
+
+
+        public CardexDAL()
+        {
+            conn = new SqlConnection(caminho);
+            conn.Open();
+
+
+        }
+        public void fecha()
+        {
+            this.conn.Close();
+
+        }
+
+
+
+
+        public void cadastrar(CardexDAL cadastrar)
+        {
+            SqlCommand cmd = new SqlCommand("insert into Cardex values (@IdProduto, @Produto, @Quantidade, @Entrada, @Validade, @saida )", conn);
+
+            cmd.Parameters.AddWithValue("@IdProduto", IdProduto);
+            cmd.Parameters.AddWithValue("Produto", Produto);
+            cmd.Parameters.AddWithValue("@Quantidade", Quantidade);
+            cmd.Parameters.AddWithValue("@Entrada", Entrada);
+            cmd.Parameters.AddWithValue("@validade", Validade);
+            cmd.Parameters.AddWithValue("@Saida", Saida);
+
+            cmd.ExecuteNonQuery();
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
